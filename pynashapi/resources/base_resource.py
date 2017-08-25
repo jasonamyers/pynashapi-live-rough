ab5dfd62070cb45c2922fc600f6765368a0d34bd
Jason Myers <jason@jasonamyers.com>
2017-08-24 19:14:25 -0500

    Base resource
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- pynashapi/resources/base_resource.py
+++ pynashapi/resources/base_resource.py
@@ -1,7 +1,7 @@
 from functools import wraps
 
-from flask import request
-from flask_restful import abort, Resource
+from flask import request, abort
+from flask_restful import Resource
 
 
 def validate_content_type(func):
